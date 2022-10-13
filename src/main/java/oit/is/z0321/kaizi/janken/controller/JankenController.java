1e7dc383f928fb5159881fa6442bc116822b147f
Nobuyuki2002 <e1b20066@oit.ac.jp>
2022-10-13 13:30:36 +0900

    add security task

--- src/main/java/oit/is/z0321/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0321/kaizi/janken/controller/JankenController.java
@@ -1,9 +1,12 @@
 package oit.is.z0321.kaizi.janken.controller;
 
+import java.security.Principal;
+import oit.is.z0321.kaizi.janken.model.Entry;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,28 +21,29 @@ import oit.is.z0321.kaizi.janken.model.Janken;
 @RequestMapping("/janken")
 public class JankenController {
 
-  /**
-   * index.htmlでPOST受付/jankenへ遷移する
-   *
-   * @param user
-   * @param model
-   * @return
-   */
-  @PostMapping
-  public String janken(@RequestParam String user, ModelMap model) {
-    model.addAttribute("user_name", user);
+  @Autowired
+  private Entry entry;
+
+  /** */
+  @GetMapping
+  public String janken(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("room", this.entry);
+    model.addAttribute("user_name", loginUser);
+    model.addAttribute("new_room", this.entry);
     return "janken.html";
   }
 
-  /**
-   * Getでアクセスされた場合
-   *
-   * @return
-   */
-  @GetMapping
-  public String janken() {
-    return "janken.html";
-  }
+  // /**
+  // * Getでアクセスされた場合
+  // *
+  // * @return
+  // */
+  // @GetMapping
+  // public String janken() {
+  // return "janken.html";
+  // }
 
   /**
    * jankenの結果を返す
@@ -48,7 +52,7 @@ public class JankenController {
    * @param model
    * @return
    */
-  @GetMapping("/game")
+  @GetMapping("game")
   public String janken(@RequestParam Integer hand, ModelMap model) {
     Janken janken = new Janken(hand);
     model.addAttribute("my_hand", janken.getMyhand());
